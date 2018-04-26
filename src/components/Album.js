7aa0f3efabc24050b956a8f9606c4be14184236d
Clint Nicholson <clintondnicholson@gmail.com>
2018-04-26 00:23:25 -0500

    checkpoint-9-bloc-jams-react: done

--- src/components/Album.js
+++ src/components/Album.js
@@ -13,12 +13,38 @@ class Album extends Component {
     this.state = {
       album : album,
       currentSong: album.songs[0],
+      currentTime:0,
+      duration: album.songs[0].duration,
       isPlaying: false
     };
 
     this.audioElement = document.createElement('audio');
     this.audioElement.src = album.songs[0].audioSrc;
     };
+
+    componentDidMount() {
+      this.eventListeners = {
+        timeupdate: e => {
+          this.setState({ currentTime: this.audioElement.currentTime });
+        },
+        durationchange: e => {
+          this.setState({ duration: this.audioElement.duration });
+        }
+      };
+      this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+      this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+    }
+
+    componentWillUnmount() {
+     this.audioElement.src = null;
+     this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+     this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+    }
+
+    setSong(song) {
+     this.audioElement.src = song.audioSrc;
+     this.setState({ currentSong: song });
+    }
 
     play() {
       this.audioElement.play();
@@ -30,20 +56,23 @@ class Album extends Component {
       this.setState({ isPlaying: false });
     }
 
-    setSong(song) {
-     this.audioElement.src = song.audioSrc;
-     this.setState({ currentSong: song });
-   }
+    handleSongClick(song) {
+       const isSameSong = this.state.currentSong === song;
 
-   handleSongClick(song) {
-     const isSameSong = this.state.currentSong === song;
+       if (this.state.isPlaying && isSameSong) {
+           this.pause();
+         } else {
+           if (!isSameSong) { this.setSong(song); }
+           this.play();
+       }
+      }
 
-     if (this.state.isPlaying && isSameSong) {
-         this.pause();
-       } else {
-         if (!isSameSong) { this.setSong(song); }
-         this.play();
-     }
+    handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
     }
 
     handleNextClick() {
@@ -53,6 +82,13 @@ class Album extends Component {
       this.setSong(newSong);
       this.play(newSong);
     }
+
+    handleTimeChange(e) {
+     const newTime = this.audioElement.duration * e.target.value;
+     this.audioElement.currentTime = newTime;
+     this.setState({ currentTime: newTime });
+    }
+
 
   render() {
     return(
@@ -88,12 +124,15 @@ class Album extends Component {
         <PlayerBar
            isPlaying={this.state.isPlaying}
            currentSong={this.state.currentSong}
+           currentTime={this.audioElement.currentTime}
+           duration={this.audioElement.duration}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
            handleNextClick={() => this.handleNextClick()}
+           handleTimeChange={(e) => this.handleTimeChange(e)}
          />
       </section>
-    );
+    )
   }
 }
 
