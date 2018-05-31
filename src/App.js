5c6de52569aa7d5a09c7e74ad852690a667d4ff9
Clint Nicholson <clintondnicholson@gmail.com>
2018-05-31 06:38:15 -0500

    checkpoint-10 added logo and bootstrap

--- src/App.js
+++ src/App.js
@@ -10,13 +10,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
+          <nav class="navbar navbar-expand-lg">
+            <img src="/Users/chapelmedia/Bloc/bloc-jams-react/public/assets/images" href="/" width="30" height="30" alt=""></img>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="/">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="/library">Library</a>
+              </div>
+            </div>
           </nav>
-          <h1>Bloc Jams</h1>
-        </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
