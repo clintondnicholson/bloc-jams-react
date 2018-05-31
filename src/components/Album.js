71d36d99ebd00b17a5367586e48e72d4bec59c0e
Chapel Media <chapelmedia@medias-imac.thechapelnet.net>
2018-05-31 06:38:15 -0500

    checkpoint-10 added logo and bootstrap

--- src/components/Album.js
+++ src/components/Album.js
@@ -116,34 +116,40 @@ class Album extends Component {
   render() {
     return(
       <section className='album'>
-        <section id="album-info">
-          <img id="album-cover-art" alt="album art" src={this.state.album.albumCover} />
-          <div className="album-details">
-            <h1 id="album-title">{this.state.album.title}</h1>
-            <h2 className="artist">{this.state.album.artist}</h2>
-            <div id="release-info">{this.state.album.releaseInfo}</div>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+            <section id="album-info">
+              <img id="album-cover-art" alt="album art" src={this.state.album.albumCover} />
+              <div className="album-details">
+                <h1 id="album-title">{this.state.album.title}</h1>
+                <h2 className="artist">{this.state.album.artist}</h2>
+                <div id="release-info">{this.state.album.releaseInfo}</div>
+              </div>
+            </section>
+            </div>
+            <table className="col-6" id="song-list">
+              <colgroup>
+                <col id="song-number-column" />
+                <col id="song-title-column" />
+                <col id="song-duration-column" />
+              </colgroup>
+              <tbody>
+                {
+                  this.state.album.songs.map( (song, index)  =>
+                    <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
+                      <td className="song-number">{index + 1}</td>
+                      <td className="ion-ios-play"></td>
+                      <td className="ion-ios-pause"></td>
+                      <td className="song-title">{song.title}</td>
+                      <td className="song-duration">{this.formatTime(song.duration)} </td>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
           </div>
-        </section>
-        <table id="song-list">
-          <colgroup>
-            <col id="song-number-column" />
-            <col id="song-title-column" />
-            <col id="song-duration-column" />
-          </colgroup>
-          <tbody>
-            {
-              this.state.album.songs.map( (song, index)  =>
-                <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
-                  <td className="song-number">{index + 1}</td>
-                  <td className="ion-ios-play"></td>
-                  <td className="ion-ios-pause"></td>
-                  <td className="song-title">{song.title}</td>
-                  <td className="song-duration">{this.formatTime(song.duration)} </td>
-                </tr>
-              )
-            }
-          </tbody>
-        </table>
+        </div>
         <PlayerBar
            isPlaying={this.state.isPlaying}
            currentSong={this.state.currentSong}
