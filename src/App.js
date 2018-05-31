c129ddb088bf5f618118c2bd0ca90cb0b6fa5daf
Chapel Media <chapelmedia@medias-imac.thechapelnet.net>
2018-05-31 07:04:40 -0500

    blocJAMS logo added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
           <nav class="navbar navbar-expand-lg">
-            <img src="/Users/chapelmedia/Bloc/bloc-jams-react/public/assets/images" href="/" width="30" height="30" alt=""></img>
+            <img src="/Users/chapelmedia/Bloc/bloc-jams-react/public/assets/images/BlocJamsWhite.png" href="/" alt=""></img>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
